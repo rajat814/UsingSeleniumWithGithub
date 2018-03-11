de9170258e6971957adf1c165a1f6e623803d06d
Rajat <Rajat@192.168.100.6>
2018-03-11 14:35:46 +0530

    First Commit
    Change-Id: If4fde8b9f49d0e6f5eaef945b5d350314eb61b96

---
+++ UsingSeleniumWithGithub/src/test/java/com/Rajat/UsingSeleniumWithGithub/Test1.java
@@ -0,0 +1,27 @@
+package com.Rajat.UsingSeleniumWithGithub;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	
+		
+		@Test
+		public void first() {
+			System.out.println("First");
+		}
+		
+		@Test
+		public void second() {
+			System.out.println("second");
+		} 
+		
+		@Test
+		public void commitTest() {
+			System.out.println("CommitTest");
+		}
+
+	}
+
+
+
